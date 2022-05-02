8fa67fcb3c4cd9c32c658a6fac9bcf3bad89ce52
xyicecream <icecream_896@163.com>
2022-05-02 18:08:39 +0800

    git提交

---
+++ p1-if.py
@@ -0,0 +1,14 @@
+# #第一种if语法
+# money = 500
+# if money > 300:
+#     print("出去嗨")
+# print("回家")
+
+# # 第二种if语法
+money = input("请输入有多少钱")
+money = int(money)
+if money > 500:
+    print("hang out")
+else:
+    print("go back")
+    
