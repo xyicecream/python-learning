abf8b8d73e3a623bc37f0a08d76153e6812ce76b
xyicecream <icecream_896@163.com>
2022-05-02 18:27:12 +0800

    学习python if语法

--- p1-if.py
+++ p1-if.py
@@ -5,10 +5,28 @@
 # print("回家")
 
 # # 第二种if语法
-money = input("请输入有多少钱")
-money = int(money)
-if money > 500:
-    print("hang out")
+# money = input("请输入有多少钱:")
+# money = int(money)
+# if money > 500:
+#     print("hang out")
+# else:
+#     print("go back")
+
+#第三种if语法 if嵌套语句
+# money = int(input("how much money:"))
+# if money >1000:
+#     if money >5000:
+#         print("buy a LV")
+#     else:
+#         print("have a nice dinner")
+# else:
+#     print("go back")
+
+#第四种if语法 
+money = int(input("how much money:"))
+if money >5000:
+     print("buy a LV")  
+elif money >1000:
+    print("eat")
 else:
     print("go back")
-    
