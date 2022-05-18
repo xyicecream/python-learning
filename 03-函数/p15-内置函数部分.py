b9cd46b1fa2d0385581c0eb8451a4d5baa5fe99e
xyicecream <icecream_896@163.com>
2022-05-18 14:12:13 +0800

    内置函数sorted/filter/map

--- 03-函数/p15-内置函数部分.py
+++ 03-函数/p15-内置函数部分.py
@@ -14,6 +14,12 @@
       1.字典: dict
       2.集合: set,frozenset
     3.相关内置函数: len, sorted,enumerate,all,any,zip,fiter,map
+2.作用域相关
+  locals： 函数会以字典形式返回当前位置的全部局部变量
+  globals：函数会以字典类型返回全部全局变量
+3.迭代器生成器相关
+  range,next,iter
+
 
 '''
 
@@ -56,20 +62,93 @@
 #     print(chr(i)+" ",end="")
 
 # all,any
-print(all([12,"呵呵","路"])) #当成and来看 true and true and true
-print(any([0,123,""])) #当成or来看
+# print(all([12,"呵呵","路"])) #当成and来看 true and true and true
+# print(any([0,123,""])) #当成or来看
 
-#enumerate
-lst = ["一二","布布","小喵","小基"]
-for index,item in enumerate(lst):
-    print(index,item)
-for i in range(len(lst)):
-    print(i,lst[i])
+# #enumerate
+# lst = ["一二","布布","小喵","小基"]
+# for index,item in enumerate(lst):
+#     print(index,item)
+# for i in range(len(lst)):
+#     print(i,lst[i])
 
-s = "xixi"
-print(hash(s)) # 一定是一个数字，想办法转化成内存地址，然后进行数据的存储 -> 字典（集合）哈希表
+# s = "xixi"
+# print(hash(s)) # 一定是一个数字，想办法转化成内存地址，然后进行数据的存储 -> 字典（集合）哈希表
 
-# dir 当前这个数据能执行哪些操作
+# # dir 当前这个数据能执行哪些操作
+
+# ========================================
+
+
+# zip 可以把多个可迭代内容合并
+#把列表1,2,3中的索引0,1,2的所有元素列在一起
+lst1 = ["赵本山","范伟","苏有朋"]
+lst2 = [40,38,42]
+lst3 = ["卖拐","耳朵大有福","情深深雨蒙蒙"]
+
+# result = zip(lst1,lst2,lst3)
+# # #print(result)
+# # for item in result:
+# #   print(item)
+
+# lst = list(result)
+# print(lst)
+
+# # locals
+# a = 188
+# print(locals()) #此时locals被写在了全局作用域范围内，此时看到的就是全局作用与中的内容
+
+# def func():
+#   a = 336
+#   print(locals()) #此时locals放在局部作用域范伟，看到的就是局部作用域的内容
+# func()
+
+# c = 12
+# print(globals()) # globals看到的是全局作用域的内容
+
+# def func():
+#   d = 33
+#   print(globals())
+# func()
+
+
+# sorted 排序
+# lst = [23,49,12,444,1,5,99]
+# s = sorted(lst,reverse = True)
+# print(s)
+
+# lst = ["卡卡","奥德赛","马里奥","塞尔达","刺客"]
+
+# # def func(item): #item对应的就是列表中的每项数据
+# #   return len(item)
+# s = sorted(lst,key = lambda x: len(x)) # 一般lambda表达式配合sorted使用
+# print(s)
+
+# lst = [{"id":1,"name":"周润发","age":18,"salary":5600},
+# {"id":2,"name":"周星驰","age":28,"salary":5111},
+# {"id":3,"name":"周海媚","age":78,"salary":5612},
+# {"id":4,"name":"周伯通","age":12,"salary":5321},
+# {"id":5,"name":"周大兴","age":35,"salary":5987},
+# {"id":6,"name":"周周","age":47,"salary":598},
+# {"id":7,"name":"周扒皮","age":8,"salary":56},
+# ]
+# #根据每个人的年龄进行排序
+# s = sorted(lst,key = lambda d : d['age'])
+# print(s)
+
+# #根据工资进行排序，从大到小
+# s2 = sorted(lst,key = lambda d : d['salary'],reverse=True)
+# print(s2)
+
+# filter 筛选
+lst = ["张无忌","张三丰","张翠山","灭绝师太","小狐仙"]
+f = filter(lambda x: not x.startswith("张"),lst)
+print(list(f))
+
+#map: 映射
+lst = [1,2,3,4,5,6,7,8,9]
+r = map(lambda x: x*x,lst)
+print(list(r))
 
 
 
